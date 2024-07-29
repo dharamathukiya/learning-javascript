9b17d5ccc973baee2cf695f5042b2d59532f3fe0
Dhara Mathukiya <83548803+dharamathukiya@users.noreply.github.com>
2024-07-29 16:44:26 +0000

    datatypes

---
+++ 01-basics/02-datatypes.js
@@ -0,0 +1,23 @@
+"use strict"  //treat all JS code as newer version
+
+// alert(3+3) //we are using NodeJS , not browser
+
+
+let name = "Dhara"
+let age = 26
+let isloggedIn = true
+
+// number => 2 to 53
+// bigint => used for big number like, stokemarket website etc.
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefine => not assign value in variable
+// symbol => unique
+
+
+// object
+
+console.log(typeof "Dhara"); // string
+console.log(typeof null);  // object
+console.log(typeof undefined); // undefined
