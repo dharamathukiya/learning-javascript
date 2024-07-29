87b785e210d39e0e123b5ed4f0aff295397a179c
Dhara Mathukiya <83548803+dharamathukiya@users.noreply.github.com>
2024-07-29 17:21:21 +0000

    convertion operation of datatype

---
+++ 01-basics/03-convertionOperation.js
@@ -0,0 +1,42 @@
+let score = 33
+let scoreTwo = "55abc"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(scoreTwo)
+console.log(typeof valueInNumber);
+console.log(valueInNumber); // NaN - Not a number
+
+//Note
+// "33" => number
+// "33abc" => NaN - not a number
+// true => 1 ; false => 0
+// null => 0
+// undefined => NaN
+
+
+
+
+//let isLogggedIn = true
+//let isLogggedIn = 1
+//let isLogggedIn = ""
+let isLogggedIn = "Dhara"
+
+let boolenIsLoggedIn = Boolean(isLogggedIn)
+console.log(boolenIsLoggedIn);
+
+
+//Note
+//1 => true ; 0 => false
+//"" => false
+//"Dhara" => true
+
+let somenumber = 33
+
+let stringnumber = String(somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
+
+
+
